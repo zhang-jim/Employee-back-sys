d5ad1ce29aa3081082e5fd4b7f3f21619b4b4b44
zhang-jim <zhang230531@gmail.com>
2025-04-15 15:49:31 +0800

    0.15.3[MVC 新增底層Controller]

---
+++ Controller/Controller.php
@@ -0,0 +1,22 @@
+<?php
+class Controller
+{
+    protected function isLoggedIn(): bool
+    {
+        session_start();
+        return isset($_SESSION['user_id']);
+    }
+    protected function jsonResponse($success, $message)
+    {
+        header('Content-Type:application/json');
+        echo json_encode(['success' => $success, 'message' => $message]);
+        exit();
+    }
+    protected function requireLogin() //需要登入，才能使用功能
+    {
+        // 有登入，不執行以下程式
+        if (!$this->isLoggedIn()) {
+            $this->jsonResponse(false, '請登入帳戶');
+        }
+    }
+}
